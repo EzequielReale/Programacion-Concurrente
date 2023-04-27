22dbc67052493b05ddafdadcc1e25494f8cf5d26
Ezequiel Reale <88700625+EzequielReale@users.noreply.github.com>
2023-04-27 11:37:46 -0300

    Add files via upload

---
+++ Repaso 1er parcial/Monitores/Combi.cs
@@ -0,0 +1,58 @@
+/*
+Resolver con monitores el siguiente problema. Una empresa tiene 40
+empleados que forman 4 equipos de trabajo (cada empleado conoce a
+priori a que equipo pertenece). La empresa tiene 4 combis para
+trasladar cada equipo a la planta de trabajo que le corresponda.
+Cada empleado al llegar sube a la combi que le corresponde a su
+equipo, cuando las 4 combis están completas cada una de ellas
+parte a la planta correspondiente. El traslado dura un tiempo
+aleatorio entre 5 y 20 minutos. Al llegar a su sector cada empleado
+realiza su trabajo y luego se retira de la planta (cada empleado
+se retira independientemente del resto).
+Nota: por simplicidad los empleados no se deben esperar para retirarse.
+*/
+
+
+monitor Combi [id: 0 to 3] {
+
+    int aBordo = 0, listos = 0;
+    cond todasCompletas, llegamos;
+
+    procedure subir() {
+        aBordo++;
+        if (aBordo < 10) wait(todasCompletas);
+        else {
+            Salida.esperarInicio();
+            signal_all(todasCompletas);
+        }
+    }
+
+    procedure viajar() {
+        listos++;
+        if (listos < 10) wait(llegamos);
+        else {
+            delay(random(5, 20) minutos);
+            signal_all(llegamos);
+        }
+    }
+}
+
+monitor Salida {
+
+    int combisCompletas = 0;
+    cond todasCompletas;
+
+    procedure esperarInicio() {
+        combisCompletas++;
+        if (combisCompletas < 4) wait(todasCompletas);
+        else signal_all(todasCompletas);
+    }
+}
+
+process Empleado [id: 0 to 39] {
+    int equipo = getEquipo();
+    Combi[equipo].subir();
+    Combi[equipo].viajar();
+    delay(8 horas) //trabaja
+    //se retira
+}
