1747569e16a9598218ce38f69ea9d4c576a25ba5
Ezequiel Reale <88700625+EzequielReale@users.noreply.github.com>
2023-05-10 22:55:11 -0300

    subo PMS ejercicio 1

---
+++ Práctica 4/PMS/Ej1.cs
@@ -0,0 +1,28 @@
+process Robot[id: 0 to R-1] {
+    text web;
+
+    while (true) {
+        web = buscarDireccionInfectada();
+        Admin!sitioEncontrado(web);
+    }
+}
+
+process Admin {
+    queue buffer;
+    text web;
+
+    do Robot?sitioEncontrado(web) -> push(buffer, web);
+    □ not empty(buffer); Analizador?disponible() -> Analizador!sitioParaAnalizar(pop(buffer));
+    od
+}
+
+process Analizador {
+    text web;
+    bool resultado;
+
+    while (true) {
+        Admin!disponible(); 
+        Admin?sitioParaAnalizar(web);
+        resultado = analizar(web); //Supongamos que después hace algo con esto
+    }
+}
