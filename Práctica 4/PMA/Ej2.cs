4c7bc19d413c0563b7133da8e8336b0469ec7a2b
Ezequiel Reale <88700625+EzequielReale@users.noreply.github.com>
2023-05-12 00:47:33 -0300

    Update Ej2.cs

--- Práctica 4/PMA/Ej2.cs
+++ Práctica 4/PMA/Ej2.cs
@@ -1,13 +1,20 @@
-chan cola[5](int), turno[P](int), comprobante[P](text);
+chan pedido(String), pedirCaja(int), nroCaja[P](int), cola[5](int), turno[P](), comprobante[P](text), termine(int);
 
 process Persona[id: 0 to P-1] {
-    int miCaja = min(cola);
+    int miCaja 
     text comp;
+    
+    send pedirCaja(id);
+    send pedido("llego");
+    receive nroCaja[id](miCaja);
 
     send cola[miCaja](id);
-    recieve turno[id](id);
+    recieve turno[id]();
     //Lo atienden
     recieve comprobante[id](comp);
+    
+    send termine(miCaja);
+    send pedido("me voy");
 }
 
 process Caja[id: 0 to 4] {
@@ -16,11 +23,27 @@ process Caja[id: 0 to 4] {
 
     while (true) {
         recieve cola[id](idP);
-        send turno[idP](idP);
+        send turno[idP]();
         //Atiende
         send comprobante[idP](comp);
     }
 }
 
-//Técnicamente podría llamar a otra persona antes de que el cliente
-//agarre el comprobante, pero igual como son canales privados funciona
+process Coordinador {
+    int idP, idC, personasEsperando[5] = ([5] 0);
+    String queHacer;
+    
+    while (true) {
+        receive pedido(QueHacer);
+        
+        if (queHacer == "llego") {
+            receive pedirCaja(idP);
+            idC = min(personasEsperando);
+            send nroCaja[idP](idC);
+            personasEsperando[idC]++;
+        } else if (queHacer == "me voy") {
+            receive termine(idC);
+            personasEsperando[idC]--;
+        }
+    }
+}
